8e68ba60d3bc0475034e072a2e8710189e5aa45e
teo67 <teogerst@gmail.com>
2021-11-16 18:47:58 -0800

    puuuuuuush

---
+++ src/main/kotlin/org/team5419/frc2022/controllers/Controller.kt
@@ -0,0 +1,11 @@
+package org.team5419.frc2022.controllers
+
+open class Controller {
+
+    open fun start() {}
+
+    open fun update() {}
+
+    open fun reset() {}
+
+}
---
+++ src/main/kotlin/org/team5419/frc2022/controllers/TeleopController.kt
@@ -0,0 +1,15 @@
+package org.team5419.frc2022.controllers
+
+object TeleopController : Controller() {
+    override fun start() {
+
+    }
+
+    override fun update() {
+
+    }
+
+    override fun reset() {
+
+    }
+}
