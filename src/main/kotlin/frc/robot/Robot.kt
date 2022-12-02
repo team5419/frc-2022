4828935f98b3ab166dc1a7e8cb0500f23406f3f8
teo67 <teogerst@gmail.com>
2022-12-01 17:32:03 -0800

    new branch!!!

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -14,7 +14,6 @@ val tab: ShuffleboardTab = Shuffleboard.getTab("Master")
 
 class Robot : TimedRobot() {
   private val m_robotContainer = RobotContainer(tab);
-  private var m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
   override fun robotInit() {
     NetworkTableInstance.getDefault().setUpdateRate(0.05)
@@ -26,29 +25,18 @@ class Robot : TimedRobot() {
   }
 
   override fun disabledInit() {
-    m_robotContainer.lightsOff();
   }
 
   override fun disabledPeriodic() {}
 
   // runs autonomous command selected in RobotContainer.kt
   override fun autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
     m_robotContainer.setDefaults();
-    if(m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-    //m_robotContainer.m_drivetrain.resetOdometry()
   }
 
   override fun autonomousPeriodic() {}
 
   override fun teleopInit() {
-    // cancel all autonomous commands when teleop starts
-    if(m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-    m_robotContainer.m_drivetrain.currentLimit = 20.0
   }
 
   override fun teleopPeriodic() {}
---
+++ src/main/kotlin/frc/robot/commands/Sample.kt
@@ -0,0 +1,26 @@
+package frc.robot.commands; 
+import frc.robot.subsystems.Drivetrain
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
+import edu.wpi.first.wpilibj.Timer
+
+class Sample() : CommandBase() {
+
+  init {
+  }
+
+  override fun initialize() {
+  }
+
+  override fun execute() {
+  }
+
+  override fun isFinished() : Boolean {
+    return true;
+  }
+
+  override fun end(interrupted: Boolean) {
+  }
+}
